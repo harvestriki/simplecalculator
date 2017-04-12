5bdf38adec09c4129348e5db78091975580d7918
riki ari <rikiari@sgo.co.id>
2017-04-12 13:36:08 +0700

    update +,-,/,* completed

--- app/src/main/java/com/haloriki/calculatorgahoel/MainActivity.java
+++ app/src/main/java/com/haloriki/calculatorgahoel/MainActivity.java
@@ -1,7 +1,9 @@
 package com.haloriki.calculatorgahoel;
 
+import android.renderscript.Double2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +14,15 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView ed_display;
 
-    private Double result;
+    private Double result = 0.0;
     private String operator;
+    private String currentOpr;
+    private Double lastNum;
+    private enum LAST_CLICKED {
+        OPERAND, OPERATOR, NONE, EQUAL
+    };
+
+    private LAST_CLICKED last_clicked = LAST_CLICKED.NONE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,37 +33,68 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void numericalClicked(View view) {
+
+        if(last_clicked == LAST_CLICKED.OPERATOR){
+            ed_display.setText("0.0");
+        }
+
         String title = ((Button) view).getText().toString();
-        String num = ed_display.getText().toString().equals("0") ? "" : ed_display.getText().toString();
+        String num = ed_display.getText().toString().equals("0.0") ? "" : ed_display.getText().toString();
         num += title;
 
         ed_display.setText(num);
-    }
-
-    double oparete(String a, String b, String op){
-        switch (op) {
-            case "+": return Double.valueOf(a) + Double.valueOf(b);
-            case "-": return Double.valueOf(a) - Double.valueOf(b);
-            case "*": return Double.valueOf(a) * Double.valueOf(b);
-            case "/": try {
-                returnt Double.ValueOf(a) / Double.value(b);
-            }catch (Exception e){
-                Log.d("")
-            }
-        }
+        last_clicked = LAST_CLICKED.OPERAND;
     }
 
     public void operatorClicked(View view) {
-        String currentNum = ((TextView) view).getText().toString();
 
-        if(!currentNum.equals("0")){
-            result = Double.valueOf(currentNum);
-
+        if(last_clicked == LAST_CLICKED.EQUAL){
+            result = 0.0;
         }
+        String currentNum = ed_display.getText().toString();
+        currentOpr = ((Button) view).getText().toString();
+
+        if(result > 0.0){
+            result = doCalc(result, Double.valueOf(currentNum), operator);
+        }else{
+            result = Double.valueOf(currentNum);
+        }
+
+        ed_display.setText(result.toString());
+        operator = currentOpr;
+        last_clicked = LAST_CLICKED.OPERATOR;
+
+    }
+
+    private Double doCalc(Double  a, Double b, String opr){
+        switch (opr){
+            case "+":
+                a += b;
+                break;
+            case "-":
+                a -= b;
+                break;
+            case "*":
+                a *= b;
+                break;
+            case "/":
+                a /= b;
+        }
+        return a;
     }
 
     public void equalClicked(View view) {
 
+        Double currentNum = Double.valueOf(ed_display.getText().toString());
+
+        if(last_clicked == LAST_CLICKED.EQUAL){
+            currentNum = lastNum;
+        }else{
+            lastNum = currentNum;
+        }
+        result = doCalc(result, Double.valueOf(currentNum), currentOpr);
+        ed_display.setText(result.toString());
+        last_clicked = LAST_CLICKED.EQUAL;
     }
 
     public void clearClicked(View view) {
@@ -62,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void clear() {
-        ed_display.setText("0");
+        result = 0.0;
+        ed_display.setText("0.0");
     }
 }
